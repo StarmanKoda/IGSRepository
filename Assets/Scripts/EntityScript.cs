9096495bf03f91fbe9c17d57ada62f533bd531d3
StarmanKoda <icarter@ltu.edu>
2024-01-11 15:59:39 -0500

    Jumping Enemy Basic Code

---
+++ Assets/Scripts/EntityScript.cs
@@ -0,0 +1,23 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityScript : MonoBehaviour
+{
+
+    public double health = 100;
+    public float moveSpeed = 1f;
+    public double atkDMG = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/JumpingEnemy.cs
+++ Assets/Scripts/JumpingEnemy.cs
@@ -1,21 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
-public class JumpingEnemy : MonoBehaviour
+public class JumpingEnemy : EntityScript
 {
-    [SerializeField] float moveSpeed;
-    private float moveDirection;
+    private float moveDirection = 1f;
     private bool facingRight = true;
+    [SerializeField] Transform groundCheckPoint;
+    [SerializeField] Transform wallCheckPoint;
+    [SerializeField] float circleRadius;
+    [SerializeField] LayerMask groundLayer;
+    private bool checkingGround;
+    private bool checkingWall;
+
+    private Rigidbody2D enemyRB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        checkingGround = Physics2D.OverlapCircle(groundCheckPoint.position, circleRadius, groundLayer);
+        checkingWall = Physics2D.OverlapCircle(wallCheckPoint.position, circleRadius, groundLayer);
+        Patrolling();
+    }
+    
+    void Patrolling()
+    {
+        if (checkingWall || checkingWall)
+        {
+            if (facingRight)
+            {
+                Flip();
+            }
+            else if (!facingRight)
+            {
+                Flip();
+            }
+        }
+        enemyRB.velocity = new Vector2(moveSpeed * moveDirection, enemyRB.velocity.y);
+        UnityEngine.Debug.Log("moving!");
+    }
+
+    void Flip()
+    {
+        moveDirection *= -1f;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
     }
 }
