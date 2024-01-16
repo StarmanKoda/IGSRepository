56c72d83c031e39cfee526f9742544822b9b13fd
StarmanKoda <icarter@ltu.edu>
2024-01-16 14:55:41 -0500

    JumpingEnemy edits

--- Assets/Scripts/Entity/Enemy/JumpingEnemy.cs
+++ Assets/Scripts/Entity/Enemy/JumpingEnemy.cs
@@ -15,11 +15,25 @@ public class JumpingEnemy : EntityScript
     private bool checkingGround;
     private bool checkingWall;
 
+    [Header("For Jumping")]
+    [SerializeField] float jumpHeight;
+    [SerializeField] Transform player;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] Vector2 boxSize;
+    public bool trackPlayerJump;
+    public float timeToJump;
+    private float jumpTimer;
+    private bool isGrounded;
+
     private Rigidbody2D enemyRB;
     // Start is called before the first frame update
     void Start()
     {
         enemyRB = GetComponent<Rigidbody2D>();
+        if (player == null)
+        {
+            player = gameObject.transform.Find("Player");
+        }
     }
 
     // Update is called once per frame
@@ -27,7 +41,18 @@ public class JumpingEnemy : EntityScript
     {
         checkingGround = Physics2D.OverlapCircle(groundCheckPoint.position, circleRadius, groundLayer);
         checkingWall = Physics2D.OverlapCircle(wallCheckPoint.position, circleRadius, groundLayer);
+        isGrounded = Physics2D.OverlapBox(groundCheck.position, boxSize, 0, groundLayer);
+        jumpTimer += Time.deltaTime;
         Patrolling();
+        if (Input.GetButtonDown("Jump") && trackPlayerJump)
+        {
+            Jump();
+        }
+        else if(!trackPlayerJump && jumpTimer >= timeToJump)
+        {
+            jumpTimer = 0;
+            Jump();
+        }
     }
     
     void Patrolling()
@@ -44,7 +69,17 @@ public class JumpingEnemy : EntityScript
             }
         }
         enemyRB.velocity = new Vector2(moveSpeed * moveDirection, enemyRB.velocity.y);
-        UnityEngine.Debug.Log("moving!");
+        //UnityEngine.Debug.Log("moving!");
+    }
+
+    void Jump()
+    {
+        float distancefromPlayer = player.position.x - transform.position.x;
+
+        if (isGrounded)
+        {
+            enemyRB.AddForce(new Vector2(distancefromPlayer, jumpHeight), ForceMode2D.Impulse);
+        }
     }
 
     void Flip()
@@ -52,5 +87,13 @@ public class JumpingEnemy : EntityScript
         moveDirection *= -1f;
         facingRight = !facingRight;
         transform.Rotate(0, 180, 0);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(groundCheckPoint.position, circleRadius);
+        Gizmos.DrawWireSphere(wallCheckPoint.position, circleRadius);
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(groundCheck.position, boxSize);
     }
 }
