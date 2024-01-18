5a5f1431541f8a50f27168ad236aef398ceeda6c
trumpetplayer2 <78563622+trumpetplayer2@users.noreply.github.com>
2024-01-18 15:00:53 -0500

    Implemented Dash and a pickup

--- Assets/Scripts/Entity/Player/Dash.cs
+++ Assets/Scripts/Entity/Player/Dash.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Dash : Upgrades
 {
-    public int dashCooldown = 3;
+    public double dashCooldown = 1.5;
     double cooldown = 0;
-    public static double speed = 3;
+    public static double speed = 50;
+    double dashTime = 0;
+    public static double dashTimeCooldown = 0.1;
+    Vector2 dash = new Vector2(0f,0f);
+
     public UpgradeEnum getId()
     {
         return UpgradeEnum.DASH;
@@ -14,10 +18,23 @@ public class Dash : Upgrades
 
     public void upgradeUpdate(GameObject obj)
     {
+
+        if (dashTime > 0)
+        {
+            dashTime -= Time.deltaTime;
+            Rigidbody2D body = obj.GetComponent<Rigidbody2D>();
+            if (obj == null) return;
+            body.velocity = dash * (Time.deltaTime * 100f);
+            if (dashTime < 0) { dashTime = 0; }
+            return;
+        }
+
         if(cooldown > 0) { 
             cooldown -= Time.deltaTime; 
             if(cooldown <0) { cooldown = 0; }
         }
+
+
 
         //Check for button press
         if (Input.GetButtonDown("Dash"))
@@ -26,7 +43,10 @@ public class Dash : Upgrades
             //Trigger Dash velocity in direction (1 = Left, -1 = Right
             Rigidbody2D body = obj.GetComponent<Rigidbody2D>();
             if (obj == null) return;
-            body.AddForce(new Vector2((float)(speed * -Input.GetAxis("Dash")), 0));
+            dash = new Vector2((float)(speed * Input.GetAxis("Dash")), 0);
+            body.velocity = dash * (Time.deltaTime * 100f);
+            cooldown = dashCooldown;
+            dashTime = dashTimeCooldown;
         }
 
     }
---
+++ Assets/Scripts/Level/UpgradeUnlocker.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeUnlocker : MonoBehaviour
+{
+    public UpgradeEnum Upg;
+    private void OnTriggerEnter2D(Collider2D player)
+    {
+        //Check if the object has an upgrade connected and if the collider can have an upgrade
+        if (player.GetComponent<UpgradeInventory>() == null) return;
+        //Unlock the upgrade (Or at least try)
+        UpgradeInventory inv = player.GetComponent<UpgradeInventory>();
+        //Determine which upgrade to give
+        inv.unlockUpgrade(UpgradeEnumMethods.getUpgrade(Upg));
+        Destroy(this.gameObject);
+    }
+}
