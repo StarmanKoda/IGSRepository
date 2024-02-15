eebeb7ec68fa9eab632343badc22c917765905f8
tpiszar <90849867+tpiszar@users.noreply.github.com>
2024-02-15 17:51:22 -0500

    Main Menu Changes

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject[] views;
+    public int curView = 0;
+    public GameObject keyboard;
+    public GameObject controller;
+    bool onController = true;
+    public Button keyBtn;
+    public Button contrBtn;
+
     public void switchScene(string sceneName)
     {
         try
@@ -17,10 +26,31 @@ public class MainMenu : MonoBehaviour
         }
     }
 
-    public void toggleView(GameObject disable)
+    public void switchControlType()
     {
-        disable.SetActive(true);
-        this.gameObject.SetActive(false);
+        if (onController)
+        {
+            onController = false;
+            controller.SetActive(false);
+            keyboard.SetActive(true);
+            keyBtn.interactable = false;
+            contrBtn.interactable = true;
+        }
+        else
+        {
+            onController = true;
+            controller.SetActive(true);
+            keyboard.SetActive(false);
+            keyBtn.interactable = true;
+            contrBtn.interactable = false;
+        }
+    }
+
+    public void toggleView(int newView)
+    {
+        views[newView].SetActive(true);
+        views[curView].SetActive(false);
+        curView = newView;
     }
 
     public void quit()
