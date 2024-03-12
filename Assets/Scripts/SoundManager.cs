67af0f689e0582a861332bc1dc636dd88b65c8a4
StarmanKoda <icarter@ltu.edu>
2024-03-12 15:53:45 -0400

    SoundManager Prefab

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource AS;
+    public AudioClip[] soundsource;
+    public bool[] blist;
+    public static SoundManager Instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for (int i = 0; i < soundsource.Length; i++)
+        {
+            if (blist[i] == true)
+            {
+                AS.PlayOneShot(soundsource[i], 1f);
+                blist[i] = false;
+                Debug.Log("Sound " + i + " Played");
+            }
+        }
+
+    }
+}
