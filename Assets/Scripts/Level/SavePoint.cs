3013ef29ee4cc890186cb71dc71c4ac5c759f306
trumpetplayer2 <78563622+trumpetplayer2@users.noreply.github.com>
2024-03-21 15:34:09 -0400

    Bullets pass through save points now

--- Assets/Scripts/Level/SavePoint.cs
+++ Assets/Scripts/Level/SavePoint.cs
@@ -7,6 +7,7 @@ public class SavePoint : MonoBehaviour
     public SaveState saveState;
     public GameObject displayBeam;
     public float beamTime = 1.5f;
+    bool active = false;
 
     public void Start()
     {
@@ -14,13 +15,20 @@ public class SavePoint : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
+        if(active) { return; }
+        if (!other.gameObject.tag.ToLower().Equals("player")) { return; }
+        if(other.gameObject.GetComponent<PlayerHealth>() == null) { return;  }
+        active = true;
+        PlayerHealth player = other.gameObject.GetComponent<PlayerHealth>();
         saveState.save();
         displayBeam.SetActive(true);
         Invoke("disableBeam", beamTime);
+        player.heal(player.maxHealth);
     }
 
     public void disableBeam()
     {
         displayBeam.SetActive(false);
+        active = false;
     }
 }
