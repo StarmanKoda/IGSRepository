6e666ab7928134052101aa55ebd600d5e988ff34
StarmanKoda <icarter@ltu.edu>
2024-03-26 15:52:11 -0400

    Forest Room 1

--- Assets/Scripts/Entity/Player/Movement.cs
+++ Assets/Scripts/Entity/Player/Movement.cs
@@ -145,6 +145,7 @@ public class Movement : MonoBehaviour
         {
             grounded = false;
             jumping = true;
+            if(SoundManager.Instance)
             SoundManager.Instance.blist[2] = true;
             rig.velocity = new Vector2(rig.velocity.x, 0);
             rig.AddForce(new Vector2(0f, jumpForce));
